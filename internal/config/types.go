0c23be9f2a9d6952c8654a4626cdfc8640662871
JulienQNN <julienqhn@proton.me>
2026-03-09 22:27:09 +0100

    chore keep original ci PR safe from go code

--- internal/config/types.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Config struct {
-	ModelName       string `mapstructure:"model"`
-	Language        string `mapstructure:"language"`
-	CommitMaxLength int    `mapstructure:"commit_max_length"`
-	PromptAddition  string `mapstructure:"prompt_addition"`
-}
