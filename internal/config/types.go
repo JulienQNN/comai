60a60b6ed437b0e1b1fcae194ff6a20bfd68e06d
JulienQNN <julienqhn@proton.me>
2026-03-16 22:27:46 +0100

    fix: remove unwanted Config field

--- internal/config/types.go
+++ internal/config/types.go
@@ -4,6 +4,5 @@ type Config struct {
 	ProviderName       string `mapstructure:"provider"`
 	ModelName          string `mapstructure:"model"`
 	Language           string `mapstructure:"language"`
-	MaxLength          string `mapstructure:"max_length"`
 	CustomInstructions string `mapstructure:"custom_instructions"`
 }
